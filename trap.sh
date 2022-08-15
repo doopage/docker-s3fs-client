dcd6910687e1934b750f3b6b0059191ee10990b0
truong.hua <truong.hua@youthdev.net>
2022-08-15 16:46:48 +0700

    Completely unmount the mount point to prevent duplicate mounting error

--- trap.sh
+++ trap.sh
@@ -4,6 +4,7 @@ exit_script() {
     SIGNAL=$1
     echo "Caught $SIGNAL! Unmounting ${AWS_S3_MOUNT}..."
     fusermount -uz "${AWS_S3_MOUNT}"
+    umount "${AWS_S3_MOUNT}"
     # shellcheck disable=SC2009
     s3fs=$(ps -o pid= -o comm= | grep s3fs | sed -E 's/\s*(\d+)\s+.*/\1/g')
     if [ -n "$s3fs" ]; then
